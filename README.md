65bb40eea2eacb8a5bd31b5019c2958fc5a3fc35
Aptech Computer Education, Uyo <contact@aptech-uyo.info>
2024-01-12 12:04:22 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # anagram-java
-Anagram Detector in Spring Boot
+An **anagram** is the result of rearranging the letters of a word to produce a new word (see [Wikipedia](https://en.wikipedia.org/wiki/Anagram)).
+
+**Note:** Anagrams are case insensitive
+
+Fix the endpoint to return the correct message using "is an anagram of" if the two arguments given are anagrams of each other; return using "is not an anagram of" otherwise.
+
+### Examples
+`"foefet"` is an anagram of `"toffee"`
+
+`"Buckethead"` is an anagram of `"DeathCubeK"`
+
+### Instructions
+- You are encouraged to try writing tests. Your solution will be run against many tests after this assessment.
+- Push your changes to the `main` branch: `git push origin main`
---
+++ src/main/java/info/aptech_uyo/anagram/AnagramApplication.java
@@ -0,0 +1,23 @@
+package info.aptech_uyo.anagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AnagramApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramApplication.class, args);
+	}
+
+	@GetMapping("/detect")
+	public String hello(
+		@RequestParam(value = "word", defaultValue = "foefet") String word,
+		@RequestParam(value = "testWord", defaultValue = "toffee") String testWord) {
+		return word + " is" + (word == testWord ? "" : " not") + " an anagram of " + testWord;
+	}
+
+}
